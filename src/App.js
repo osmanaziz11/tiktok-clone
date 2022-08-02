4a5ff98bb23ad274d22408c63037c02da4ae3ecf
Osman Aziz <osmanaziz012@gmail.com>
2022-08-02 16:03:04 +0500

    interface done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,16 @@
+import './App.css';
+import Video from './components/video';
+
 const App = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    //    Main Container
+    <div className="app h-screen w-screen bg-black grid place-items-center">
+      {/* video container  */}
+      <div className="video__container h-4/5 w-96 rounded shadow overflow-scroll snap-y snap-mandatory">
+        <Video />
+        <Video />
+      </div>
+    </div>
+  );
 };
 export default App;
