1dae897fa87796e87387d4cf66f481ed146fe21d
Osman Aziz <osmanaziz012@gmail.com>
2022-09-18 19:49:40 +0500

    Navigation and Newsfeed box inside app fixed

--- src/App.js
+++ src/App.js
@@ -7,10 +7,15 @@ const App = () => {
     //    Main Container
     <div className="main__container h-screen w-screen bg-black grid place-items-center ">
       {/* Content Container  */}
-      <div className="app relative h-4/5 w-96 rounded shadow overflow-scroll snap-y snap-mandatory">
-        <Newsfeed />
-        <Newsfeed />
-        <Navigation />
+      <div className="app__container relative  w-96 ">
+        {/* Application Content  */}
+        <div className="content__container w-full h-5/6 rounded-t shadow overflow-scroll snap-y snap-mandatory">
+          <Newsfeed></Newsfeed>
+        </div>
+        {/* Navigation Controls  */}
+        <div className="w-full nav__container bg-slate-100 rounded-b">
+          <Navigation></Navigation>
+        </div>
       </div>
     </div>
   );
--- src/components/newsfeed/Newsfeed.js
+++ src/components/newsfeed/Newsfeed.js
@@ -1,38 +1,12 @@
-import { useRef, useState } from 'react';
-import { Footer } from './components/Footer';
-import Options from './components/Options';
-import Watermark from './components/Watermark';
+import Video from './components/video';
 
-const Video = () => {
-  const [Playing, setPlaying] = useState(false);
-  const videoRef = useRef();
-
-  const handleVideoPlayer = () => {
-    if (Playing) {
-      videoRef.current.pause();
-      setPlaying(false);
-    } else {
-      videoRef.current.play();
-      setPlaying(true);
-    }
-  };
-
+const Newsfeed = () => {
   return (
-    <div className="bg-slate-800 w-full h-full relative">
-      <Watermark />
-      <video
-        src="https://res.cloudinary.com/ozecloud/video/upload/v1659429801/studioink-media-assects/images/home/video_ui9o0g.webm"
-        className=" w-full h-full object-fill  cursor-pointer"
-        loop
-        muted
-        onClick={handleVideoPlayer}
-        ref={videoRef}
-      ></video>
-      {/* options  */}
-      <Options />
-      {/* Footer  */}
-      <Footer />
-    </div>
+    <>
+      <Video />
+      <Video />
+      <Video />
+    </>
   );
 };
-export default Video;
+export default Newsfeed;
